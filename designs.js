477355aee2a8415ba8aefd6f2c32615cbe369c02
JSONRete <jsonrete@gmail.com>
2022-07-18 23:59:47 -0600

    setting up size picker

--- designs.js
+++ designs.js
@@ -1,10 +1,16 @@
+const pickSize = document.querySelector("#sizePicker")
+
 // Select color input
 // Select size input
+pickSize.addEventListener("submit", function (event) {
+    event.preventDefault();
+    makeGrid();
+});
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+// function makeGrid() {
 
-// Your code goes here!
+// // Your code goes here!
 
-}
+// }
