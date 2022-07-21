54394c3641173213f4c80d457af99c3cf721b97a
JSONRete <jsonrete@gmail.com>
2022-07-20 19:15:17 -0600

    currenly generating backwards grid table

--- designs.js
+++ designs.js
@@ -1,18 +1,34 @@
-// const pickSize = document.querySelector("#sizePicker")
-// const table = document.getElementById('pixelCanvas')
+// const pickSize = document.querySelector("sizePicker")
+const pickSize = document.getElementById("sizePicker")
 
-// // Select color input
-// // Select size input
-// pickSize.addEventListener("submit", function (event) {
-//     event.preventDefault();
-//     makeGrid();
-// });
+// Select color input
+// Select size input
+pickSize.addEventListener("submit", function (event) {
+    event.preventDefault();
+    makeGrid();
+});
+// }, false);
 
-// // When size is submitted by the user, call makeGrid()
+// When size is submitted by the user, call makeGrid()
 
-// function makeGrid() {
+function makeGrid() {
+    const fullTable = document.getElementById("pixelCanvas");
+    const xRow = document.getElementById("inputWidth").value;
+    const yColumn = document.getElementById("inputHeight").value;
+    console.log(`Height ${yColumn} and width: ${xRow}`);
+fullTable.innerHTML = "";
+let table = document.createElement("table")
+for (let row = 0; row < xRow; row++) {
+    let tr = document.createElement("tr");
+    for (let column = 0; column < yColumn; column++){
+        let td = document.createElement("td")
+        td.appendChild(document.createTextNode(""));
+        tr.appendChild(td);
+    }
+    table.appendChild(tr)
 
-// for (let index = 0; index < 60; index++)
-    
+}
+fullTable.appendChild(table)
 
-// }
+}
+
