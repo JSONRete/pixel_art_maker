ec16b030fa2bcd31f47a313fd1cbd520500ed10b
JSONRete <jsonrete@gmail.com>
2022-07-21 21:56:26 -0600

    tighten-up code

--- designs.js
+++ designs.js
@@ -1,34 +1,33 @@
-// const pickSize = document.querySelector("sizePicker")
 const pickSize = document.getElementById("sizePicker")
-
 // Select color input
 // Select size input
+
+// When size is submitted by the user, call makeGrid()
 pickSize.addEventListener("submit", function (event) {
     event.preventDefault();
     makeGrid();
 });
-// }, false);
-
-// When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
     const fullTable = document.getElementById("pixelCanvas");
     const xRow = document.getElementById("inputWidth").value;
     const yColumn = document.getElementById("inputHeight").value;
-    console.log(`Height ${yColumn} and width: ${xRow}`);
-fullTable.innerHTML = "";
-let table = document.createElement("table")
-for (let row = 0; row < xRow; row++) {
-    let tr = document.createElement("tr");
-    for (let column = 0; column < yColumn; column++){
-        let td = document.createElement("td")
-        td.appendChild(document.createTextNode(""));
-        tr.appendChild(td);
-    }
-    table.appendChild(tr)
 
+
+fullTable.innerHTML = "";
+let div = document.createElement("div")
+for (let column = 0; column < yColumn; column++){
+    let tr = document.createElement("tr")
+    for (let row = 0; row < xRow; row++) {
+        let td = document.createElement("td");
+            td.appendChild(document.createTextNode(""));
+            tr.appendChild(td);
+    }
+    div.appendChild(tr)
 }
-fullTable.appendChild(table)
+fullTable.appendChild(div)
+
+
 
 }
 
